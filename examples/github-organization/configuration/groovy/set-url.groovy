6234f16a704bc03e1d87bcc538c524294b4b478a
Martin Grunbaum <martin.grunbaum@pleo.io>
2021-01-25 16:49:36 +0100

    Minor tweaks

--- examples/github-organization/configuration/groovy/set-url.groovy
+++ examples/github-organization/configuration/groovy/set-url.groovy
@@ -1,5 +1,5 @@
 import jenkins.model.JenkinsLocationConfiguration
 
 def jlc = JenkinsLocationConfiguration.get()
-jlc.setUrl("http://127.0.0.1:8080")
+jlc.setUrl("http://poseidon.staging.pleo.io")
 jlc.save()
