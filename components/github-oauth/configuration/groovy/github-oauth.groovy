750b54971c4afbef7321eef95339860248120290
Martin Grunbaum <martin.grunbaum@pleo.io>
2021-01-26 19:58:22 +0100

    Work work

--- components/github-oauth/configuration/groovy/github-oauth.groovy
+++ components/github-oauth/configuration/groovy/github-oauth.groovy
@@ -5,6 +5,7 @@
 
 import org.jenkinsci.plugins.GithubAuthorizationStrategy
 import org.jenkinsci.plugins.GithubSecurityRealm
+import jenkins.model.Jenkins
 
 def isValidString = { value ->
     if (value != null && value instanceof String && value.trim() != "") {
@@ -16,57 +17,31 @@ def isValidString = { value ->
 def env = System.getenv()
 def jenkins = Jenkins.getInstance()
 
-// Configure Github Authentication Security Realm and GitHub Committer Authorization Strategy
-def configureGitHubAuthorizationStrategy = { clientId, clientSecret, admins, orgNames, oauthScopes ->
-    if (!isValidString(clientId)) {
-        throw new Throwable("'JENKINS_GITHUB_CLIENT_ID' is required for GitHub Authorization Strategy")
-    }
-    if (!isValidString(clientSecret)) {
-        throw new Throwable("'JENKINS_GITHUB_CLIENT_SECRET' is required for GitHub Authorization Strategy")
-    }
-    if (!isValidString(admins)) {
-        throw new Throwable("'JENKINS_GITHUB_ADMINS' is required for GitHub Authorization Strategy")
-    }
-    if (!isValidString(orgNames)) {
-        throw new Throwable("'JENKINS_GITHUB_ORG_NAMES' is required for GitHub Authorization Strategy")
-    }
-    if (!isValidString(oauthScopes)) {
-        throw new Throwable("'JENKINS_GITHUB_OAUTH_SCOPES' is required for GitHub Authorization Strategy")
-    }
-
-    // https://github.com/mocleiri/github-oauth-plugin/blob/master/src/main/java/org/jenkinsci/plugins/GithubSecurityRealm.java
-    def githubSecurityRealm = new GithubSecurityRealm(
-            "https://github.com",
-            "https://api.github.com",
-            clientId,
-            clientSecret,
-            oauthScopes
-    )
-
-    // https://github.com/jenkinsci/github-oauth-plugin/blob/master/src/main/java/org/jenkinsci/plugins/GithubAuthorizationStrategy.java
-    def githubAuthorizationStrategy = new GithubAuthorizationStrategy(
-            admins,    /*adminUserNames*/
-            false,     /*authenticatedUserReadPermission*/
-            true,      /*useRepositoryPermissions*/
-            false,     /*authenticatedUserCreateJobPermission*/
-            orgNames,  /*organizationNames*/
-            true,      /*allowGithubWebHookPermission*/
-            false,     /*allowCcTrayPermission*/
-            false,     /*allowAnonymousReadPermission*/
-            false      /*allowAnonymousJobStatusPermission*/
-    )
-
-    jenkins.setSecurityRealm(githubSecurityRealm)
-    jenkins.setAuthorizationStrategy(githubAuthorizationStrategy)
-    jenkins.save()
-}
-
-configureGitHubAuthorizationStrategy(
-        env.JENKINS_GITHUB_CLIENT_ID,
-        env.JENKINS_GITHUB_CLIENT_SECRET,
-        env.JENKINS_GITHUB_ADMINS,
-        env.JENKINS_GITHUB_ORG_NAMES,
-        env.JENKINS_GITHUB_OAUTH_SCOPES
+def githubSecurityRealm = new GithubSecurityRealm(
+    GithubSecurityRealm.DEFAULT_WEB_URI,
+    GithubSecurityRealm.DEFAULT_API_URI,
+    System.getenv('JENKINS_GITHUB_CLIENT_ID'),
+    System.getenv('JENKINS_GITHUB_CLIENT_SECRET'),
+    GithubSecurityRealm.DEFAULT_OAUTH_SCOPES
 )
 
+def githubAuthorizationStrategy = new GithubAuthorizationStrategy(
+    System.getenv('JENKINS_GITHUB_ADMINS'), /* adminUserNames */
+    false,     /*authenticatedUserReadPermission*/
+    true,      /*useRepositoryPermissions*/
+    false,     /*authenticatedUserCreateJobPermission*/
+    System.getenv('JENKINS_GITHUB_ORG_NAMES'), /*organizationNames*/
+    true,      /*allowGithubWebHookPermission*/
+    false,     /*allowCcTrayPermission*/
+    false,     /*allowAnonymousReadPermission*/
+    false      /*allowAnonymousJobStatusPermission*/
+)
+
+jenkins.setSecurityRealm(githubSecurityRealm)
+jenkins.setAuthorizationStrategy(githubAuthorizationStrategy)
 jenkins.save()
+
+GithubSecurityRealm realm = (GithubSecurityRealm) jenkins.getSecurityRealm()
+// TODO: Remove below debug
+println "Oauth scopes: ${realm.getOauthScopes()}"
+println "hasScope read:org: ${realm.hasScope('read:org')} user: ${realm.hasScope('user')}"
